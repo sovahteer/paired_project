44d66a3eec30e7dddc215c78d11eb53822790ed2
Euan <thecableton@gmail.com>
2018-08-24 14:06:20 +0100

    Added create paddock function. All the errors

--- src/main/java/controllers/PaddockController.java
+++ src/main/java/controllers/PaddockController.java
@@ -1,16 +1,21 @@
 package controllers;
 
 import db.DBHelper;
+import models.dinosaurs.Dinosaur;
+import models.enums.DietryType;
 import models.enums.DinosaurType;
 import models.paddocks.Paddock;
+import models.parks.Park;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class PaddockController {
     public PaddockController(){ setupEndpoints();}
@@ -30,6 +35,17 @@ public class PaddockController {
             model.put("DinoType", dinosaurTypes);
             model.put("templates", "template/layout.vtl");
             return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        post("/paddocks", (req, res) -> {
+            int parkId = Integer.parseInt(req.queryParams("park"));
+            DietryType dietryType = DietryType.valueOf(req.queryParams("diet_type"));
+            Park park = DBHelper.find(parkId, Park.class);
+            String name = req.queryParams("name");
+            Paddock paddock = new Paddock(name, park, dietryType);
+            DBHelper.save(paddock);
+            res.redirect("/paddocks");
+            return null;
         }, new VelocityTemplateEngine());
     }
 
