92623fbc665560fa5fde302765d3f2f6a9757fcd
Euan <thecableton@gmail.com>
2018-08-25 12:20:17 +0100

    Added Edit and Delete Functions

--- src/main/java/controllers/PaddockController.java
+++ src/main/java/controllers/PaddockController.java
@@ -40,10 +40,24 @@ public class PaddockController {
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
 
+        get("/paddocks/:id", (req, res) -> {
+            String strId = req.params(":id");
+            Integer intId = Integer.parseInt(strId);
+            Paddock paddock = DBHelper.find(intId, Paddock.class);
+
+            Map<String, Object> model = new HashMap<>();
+
+            model.put("paddock", paddock);
+            model.put("template", "templates/paddocks/show.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
         get("/paddocks/:id/edit", (req, res) -> {
             String strId = req.params(":id");
             Integer intId = Integer.parseInt(strId);
             Paddock paddock= DBHelper.find(intId, Paddock.class);
+            List<Park> parks = DBHelper.getAll(Park.class);
+            Park park = parks.get(0);
             DietryType[] dietryTypes = DietryType.values();
 
             Map<String, Object> model = new HashMap<>();
@@ -64,6 +78,30 @@ public class PaddockController {
 
             res.redirect("/paddocks");
             return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        post("/paddocks/:id", (req, res) -> {
+            String strId = req.params(":id");
+            Integer intId = Integer.parseInt(strId);
+            Paddock paddock = DBHelper.find(intId, Paddock.class);
+            DietryType dietryType = DietryType.valueOf(req.queryParams("dietryType"));
+            String name = req.queryParams("name");
+
+            paddock.setName(name);
+            paddock.setDietryType(dietryType);
+
+
+            DBHelper.update(paddock);
+            res.redirect("/paddocks");
+            return null;
+        }, new VelocityTemplateEngine());
+
+        post ("/paddocks/:id/delete", (req, res) -> {
+            int id = Integer.parseInt(req.params(":id"));
+            Paddock paddockToDelete = DBHelper.find(id, Paddock.class);
+            DBHelper.delete(paddockToDelete);
+            res.redirect("/paddocks");
+            return null;
         }, new VelocityTemplateEngine());
     }
 
