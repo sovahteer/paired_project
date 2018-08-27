b20564988e26301b3509c1d12dd66715425326c4
Joanna Gora <joanna.gora@protonmail.com>
2018-08-27 22:19:25 +0100

    add rampage method

--- src/main/java/controllers/AdminController.java
+++ src/main/java/controllers/AdminController.java
@@ -1,12 +1,18 @@
 package controllers;
 
+import db.DBHelper;
+import models.paddocks.Paddock;
+
+import models.visitors.Visitor;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 import static spark.Spark.get;
+
 
 public class AdminController {
     public AdminController() {
@@ -19,6 +25,21 @@ public class AdminController {
             model.put("template", "templates/admin/index.vtl");
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
+
+        get("/admin/:id/system_check", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int paddockId = Integer.parseInt(req.params(":id"));
+            Paddock paddock = DBHelper.find(paddockId, Paddock.class);
+            paddock.rampageCheck();
+            int numberOfVisitors = DBHelper.getAll(Visitor.class).size();
+            Random random = new Random();
+            int randomNumber = random.nextInt(numberOfVisitors + 1);
+            model.put("paddock", paddock);
+            model.put("randomNumber", randomNumber);
+            model.put("template", "templates/admin/system_check.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
     }
+
 
 }
