88b37a7c768fb17effd0c613673abaf949d174ab
Euan <thecableton@gmail.com>
2018-08-28 10:48:37 +0100

    paddocks now show on map

--- src/main/java/controllers/ParkController.java
+++ src/main/java/controllers/ParkController.java
@@ -1,9 +1,12 @@
 package controllers;
 import db.DBPaddock;
+import models.paddocks.Paddock;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.get;
@@ -16,9 +19,24 @@ public class ParkController {
     private static void startEndpoints() {
         get("/park", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-            DBPaddock.filterByCanVisit();
+            List<Paddock> allowedToVisit = DBPaddock.filterByCanVisit();
+            Collections.shuffle(allowedToVisit);
+            List<Paddock> paddocks =  listOfVisitablePaddocks();
+            model.put("paddocks", paddocks);
             model.put("template", "templates/park/index.vtl");
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
+    }
+
+    private static List<Paddock> listOfVisitablePaddocks(){
+        List<Paddock> allowedToVisit = DBPaddock.filterByCanVisit();
+        Collections.shuffle(allowedToVisit);
+        List<Paddock> paddocks;
+        if (allowedToVisit.size() < 8){
+            paddocks = allowedToVisit.subList(0, allowedToVisit.size());
+        } else {
+            paddocks = allowedToVisit.subList(0, 8);
+        }
+        return paddocks;
     }
 }
